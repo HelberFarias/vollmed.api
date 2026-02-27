3b2d18e65c4652602f699a1adab5ec2babe11468
Helber Farias <helberfarias.ptpro@gmail.com>
2026-02-27 16:07:01 -0300

    criado testes para controller de consultas no metodo agendar

---
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -0,0 +1,19 @@
+package med.voll.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest //anotação para usar nos controllers
+class ConsultaControllerTest {
+
+    @Test
+    @DisplayName ("deveria devolver http400 quando informações estao invalidas analisadas pelo Bean")
+    void agendar_cenario1(){
+        
+
+    }
+
+
+}
